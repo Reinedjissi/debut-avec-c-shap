c9a9eb7b92ad5781b40b318839902b157e81442a
reine djissi <reinedjissi211@gmail.com>
2025-01-02 22:20:08 +0000

    boucles et class en cs

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Hello, World!");
+Console.Beep();
+Console.Title = "fristApp";
+Console.Read ();
+
+
+int nb = 2004 ;
+float pi = 3.14f;
+Console.WriteLine (nb);
+nb = (int) pi ;
+Console.WriteLine (nb);*/
+
+
+/*int number = 20 ;
+switch (number <=20){
+    case 20:
+        Console.WriteLine("C'est 20");
+  break
+}*/
+
+
+       /*bool continuer = true;
+
+        while (continuer)
+        {
+            Console.Write("Veuillez entrer votre nom : ");
+            string nom = Console.ReadLine();
+
+            Console.Write("Veuillez entrer votre âge : ");
+            string age = Console.ReadLine();
+
+            
+            Console.WriteLine($"Bonjour {nom} et bienvenue ! \nTon nom est : {nom} et tu as {age} ans.");
+
+           
+            Console.WriteLine("\nVoulez-vous entrer des informations pour un autre utilisateur ? (1: Oui, 2: Non)");
+            int choix;
+
+            while (!int.TryParse(Console.ReadLine(), out choix) || (choix != 1 && choix != 2))
+            {
+                Console.Write("Choix invalide. Veuillez entrer 1 pour Oui ou 2 pour Non : ");
+            }
+
+            if (choix == 1)
+            {
+                // Demande des informations pour un autre utilisateur
+                Console.Write("Veuillez entrer son nom : ");
+                string noms = Console.ReadLine();
+
+                Console.Write("Veuillez entrer son âge : ");
+                string ages = Console.ReadLine();
+
+                // Afficher les informations
+                Console.WriteLine($"Heureux de vous revoir {nom} ! \nTon utilisateur s'appelle : {noms} et il a {ages} ans.");
+            }
+            else
+            {
+                continuer = false;
+            }
+        }
+
+        Console.WriteLine("Merci d'avoir utilisé le programme !");*/
+
+        //boucle while
+ Console.Write("hello DJISSI: "); 
+        string nom = Console.ReadLine();
+        int i = 0;
+
+        while (i < 5) 
+        {
+            Console.WriteLine(nom); 
+            i++;
+        }
